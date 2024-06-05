990d28143be3bdd977713e53c04c4ed34fd43029
omarrris <omarr.sheikh@gmail.com>
2024-06-04 23:35:13 -0700

    update home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getTeams } from '../services/api';
 
 const Home = () => {
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    const fetchTeams = async () => {
+      const data = await getTeams();
+      setTeams(data.data);
+    };
+
+    fetchTeams();
+  }, []);
+
   return (
     <div>
       <h1>Team Performance Analyzer</h1>
       <p>Select a team and season to analyze their performance.</p>
+      <ul>
+        {teams.map(team => (
+          <li key={team.id}>{team.full_name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
