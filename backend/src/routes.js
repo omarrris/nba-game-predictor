862e68999a511c72b4683c09bd634d1da02946be
omarrris <omarr.sheikh@gmail.com>
2024-06-04 23:51:49 -0700

    update routes for players

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getTeams, getTeamStats, getPlayerStats } = require('./controllers');
+const { getTeams, getTeamStats, getPlayerStats, getPlayers } = require('./controllers');
 
 const router = express.Router();
 
 router.get('/teams', getTeams);
 router.get('/team/:id/stats', getTeamStats);
 router.get('/player/:id/stats', getPlayerStats);
+router.get('/players', getPlayers);
 
 module.exports = router;
